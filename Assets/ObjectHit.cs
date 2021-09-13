7a0fe09e4819d2e585402598dad4dbbb38f3c9b8
Michael Lane <mlane@tidel.com>
2021-09-12 19:36:55 -0500

    Yeah

---
+++ Assets/ObjectHit.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHit : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Fucker hit me");
+    }
+}
+    
